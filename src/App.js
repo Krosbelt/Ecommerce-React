6687082dd7a15b8e27635f8cc9af55314f5d1cd5
Krosbelt <karenvane129@gmail.com>
2022-03-14 10:58:37 -0500

    Modificando rutas img

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
+import Home from './components/Home';
+import Products from './components/Products';
+import CheckoutPage from './components/CheckoutPage'
+import Checkout from './components/CheckoutForm/Checkout'
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home />}/>
+          <Route path='/productos' element={<Products />}/>
+          <Route path='/carrito' element={<CheckoutPage />}/>
+          <Route path='compra' element={<Checkout />}/>
+          <Route path='*' element={ <Navigate replace to="/" /> } />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
